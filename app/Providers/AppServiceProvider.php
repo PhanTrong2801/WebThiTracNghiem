cade85f334140eeaf55c317b42f2f095f7b77a43
TrongPhan <phanthanhtrong66@gmail.com>
2026-02-28 20:22:52 +0700

    init2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
     }
 
     /**
@@ -20,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // Vite::prefetch(concurrency: 3);
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
